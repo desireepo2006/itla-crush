0d20303ee6699e4afd62165e0b07239dcb42b1be
desireepo2006 <desireepo2006@gmail.com>
2026-08-16 15:13:41 -0400

    feat(auth): implementar contexto de autenticacion y persistencia de sesion

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import { Feed } from './pages/Feed'
 import { NewConfession } from './pages/NewConfession'
 import { Register } from './pages/Register'
 import { Login } from './pages/Login'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/nueva" element={<NewConfession />} />
-          <Route path="/registro" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/nueva" element={<NewConfession />} />
+            <Route path="/registro" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
